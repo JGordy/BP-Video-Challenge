53bf281f56a51d620be7352b18a79bc39c20d3fb
Joseph Gordy <jgordy2424@gmail.com>
2024-11-09 14:35:38 -0500

    Add title, description, and duration to VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,16 +1,27 @@
 const VideoCard = ({
     description,
     images,
-    publishDate,
-    shareUrl,
     subtitle,
     title,
     youtubeId,
+    onVideoCardClick
 }) => {
+    const handleVideoCardClick = () => {
+        onVideoCardClick(youtubeId)
+    };
     return (
-        <div className="video-card">
-            <h5>{title}</h5>
-            <p>{subtitle}</p>
+        <div
+            className="video-card"
+            onClick={handleVideoCardClick}
+        >
+            <img src={images.large} />
+            <div className="video-card-body">
+                <p className="video-title">{title}</p>
+                <p className="video-description">{description}</p>
+            </div>
+            <div className="video-card-footer">
+                <p className="video-duration">{subtitle}</p>
+            </div>
         </div>
     );
 };
