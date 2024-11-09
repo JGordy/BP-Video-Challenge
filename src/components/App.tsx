f3344ae4fbb4e060c2158813d7f16af26b0062c6
Joseph Gordy <jgordy2424@gmail.com>
2024-11-08 21:37:46 -0500

    Render list of video cards

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 
-import { CategoryContainer } from './CategoryContainer';
+import CategoryContainer from './CategoryContainer';
+import VideoCard from './VideoCard';
 
 export const App = () => {
 
     const [seriesDetails, setSeriesDetails] = useState({});
-    const { videoCategory } = seriesDetails;
+    const { videoCategory = {} } = seriesDetails;
+    const { videos = [] } = videoCategory;
+
 
     useEffect(() => {
         const fetchSeriesDetails = async () => {
@@ -26,11 +29,19 @@ export const App = () => {
             <div className="hero-container">
                 <div id="logo">BibleProject</div>
                 <div id="hero-content">
-                    {videoCategory && <CategoryContainer videoCategory={videoCategory} />}
+                    {videoCategory && <CategoryContainer {...videoCategory} />}
                     <div>
                         VideoPlayer stub
                     </div>
                 </div>
+            </div>
+            <div id="video-list-container">
+                {videos.length && videos.map((video) => (
+                    <VideoCard
+                        key={video.id}
+                        {...video}
+                    />
+                ))}
             </div>
         </main>
     );
