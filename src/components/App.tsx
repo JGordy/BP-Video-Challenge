2b113cd96ce9d9b6edc7724573a6adfb9001abbc
Joseph Gordy <jgordy2424@gmail.com>
2024-11-08 20:13:58 -0500

    Fetch video details

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from 'react';
+
 export const App = () => {
-  return <h1>BibleProject</h1>;
+
+    const [seriesDetails, setSeriesDetails] = useState({});
+    const { videoCategory } = seriesDetails;
+
+    useEffect(() => {
+        const fetchSeriesDetails = async () => {
+            const response = await fetch('api/data.json');
+
+            const { data } = await response.json();
+
+            if (data) {
+                setSeriesDetails(data);
+            }
+        };
+
+        fetchSeriesDetails();
+    }, []);
+
+    return (
+        <main>
+            <h1>BibleProject</h1>
+        </main>
+    );
 };
