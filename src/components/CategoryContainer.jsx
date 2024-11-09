55a476baf6e3db92a93dbfcd9d4b7fb47d8d47db
Joseph Gordy <jgordy2424@gmail.com>
2024-11-08 21:23:13 -0500

    Update category container className

--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -2,10 +2,10 @@ export const CategoryContainer = ({ videoCategory }) => {
     const { title, description } = videoCategory;
 
     return (
-        <>
+        <div className="category-details">
             <h1>{title}</h1>
             <hr />
             <p>{description}</p>
-        </>
+        </div>
     )
 };
