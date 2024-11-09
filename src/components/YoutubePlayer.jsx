b194f2dc92b5e71a0d4eed1f97e6bca026d7ed06
Joseph Gordy <jgordy2424@gmail.com>
2024-11-09 15:19:05 -0500

    Add stub of youtube player

---
+++ src/components/YoutubePlayer.jsx
@@ -0,0 +1,25 @@
+const YoutubePlayer = ({ youtubeId, title }) => {
+    console.log({ youtubeId, title });
+
+    return (
+        <div className="video-responsive">
+            <iframe
+                width="400"
+                height="200"
+                allow="accelerometer; 
+                    autoplay; 
+                    clipboard-write; 
+                    encrypted-media; 
+                    gyroscope; 
+                    picture-in-picture; 
+                    web-share"
+                src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1`}
+                frameborder="0"
+                title={title}
+                allowfullscreen
+            />
+        </div>
+    );
+};
+
+export default YoutubePlayer;
