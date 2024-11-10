d796691f768b9503edadb276fac6e80ba292ab2f
Joseph Gordy <jgordy2424@gmail.com>
2024-11-09 20:35:19 -0500

    Add useDebugMode custom hook

---
+++ src/hooks/useDebugMode.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const useDebugMode = () => {
+    const [debugMode, setDebugMode] = useState(false);
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search);
+        console.log({ queryParams: queryParams.entries() });
+
+        const paramsObj = {};
+        for (const [key, value] of queryParams.entries()) {
+            paramsObj[key] = value;
+        }
+
+        if (paramsObj?.debug === 'true') {
+            setDebugMode(true);
+        }
+    }, []);
+
+    return debugMode;
+};
+
+export default useDebugMode;
